e7ff622f16a48c006b808ddd5acef69e90ee10fb
saranesh <158140629+Speedwolf02@users.noreply.github.com>
2026-05-21 11:22:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ from pyrogram.errors import FloodWait
 #  CONFIG  — edit these
 # ═══════════════════════════════════════════════════════
 API_ID         = 24435985
-API_HASH       = " 0fec896446625478537e43906a4829f8"
+API_HASH       = "0fec896446625478537e43906a4829f8"
 BOT_TOKEN      = "7722665729:AAHDh8TAiv4uv9nfgwoBWZHexD1VEaCdx7Y"
 
 CHANNEL_TAG    = "@Anime_warrior_tamil"
